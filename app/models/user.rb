8d0f88c5a9650ac12ae4fb58059e220e9dd43dd2
lamiasristy <lamiasristy@gmail.com>
2020-11-18 17:52:47 +0100

    Added some extra styllings for the views.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable 
-
+  validates :name, presence: true
   has_many :transactions, class_name: 'Transaction', foreign_key: 'author_id'
 end
