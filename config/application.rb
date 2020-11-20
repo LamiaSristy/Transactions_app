9a7f11ea598ed834e38c26b2106c7691db10abeb
lamiasristy <lamiasristy@gmail.com>
2020-11-20 02:11:53 +0100

    Making some change in application.rb file

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module TransactionsApp
   class Application < Rails::Application   
-    
+    config.assets.initialize_on_precompile = false
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
