d9c19564ec478d865fb606c0f10199a4b519eaac
lamiasristy <lamiasristy@gmail.com>
2020-11-20 01:31:43 +0100

    Changes in application.rb file

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module TransactionsApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.assets.initialize_on_precompile = false
   end
 end
